5dfe03205c8bbb9a5aa9b3b810a33de953fa414b
zhangyunxiang <zhangyunxiang@p1.com>
2021-07-09 16:07:56 +0800

    优化

--- app/src/main/java/com/owl/kotlin/code/5-3 协程进阶.kt
+++ app/src/main/java/com/owl/kotlin/code/5-3 协程进阶.kt
@@ -17,16 +17,16 @@ fun main() {
 
 @InternalCoroutinesApi
 fun flow1() {
-    // 创建流
     //原生创建
     //flowOf、asFlow(数组、列表)
-//    flowOf("2","3","4")
-//    listOf(2,4,6).asFlow()
-    //改变数据发射的线程 flowOn - subscribeOn(RxJava)
-    //消费数据的线程是 启动协程时指定的调度器
-    //collect 订阅时，flow 才发射
-    //有序发射数据，所有的数据都会经过 filter、map、collect
-    //Flow 采用和协程一样的协作取消
+    //flowOf("2","3","4")
+    //listOf(2,4,6).asFlow()
+    //1.改变数据发射的线程 flowOn - subscribeOn(RxJava)
+    //2.collect 消费数据的线程是 启动协程时指定的调度器
+    //3.collect 订阅时，flow 才发射
+    //4.有序发射数据，所有的数据都会经过 filter、map、collect
+    //5.Flow 采用和协程一样的协作取消
+    //back Pressure
     kotlinx.coroutines.runBlocking {
         flow {
             for (i in 1..10) {
